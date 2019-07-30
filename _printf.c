4ee77c9d46373bb5d2c7e3d3f9ecd79a03ac504b
jorgezafra94 <947@holbertonschool.com>
2019-07-30 18:04:09 +0000

    [Update] _printf.c: add function print_p to get address

--- _printf.c
+++ _printf.c
@@ -28,7 +28,7 @@ int (*getfunction(char c))(va_list a, char *s, int *index)
 		{'c', print_c}, {'s', print_s}, {'%', print_por}, {'i', print_id},
 		{'d', print_id}, {'b', print_bin}, {'u', print_u}, {'o', print_o},
 		{'x', print_x}, {'X', print_X}, {'S', print_S}, {'R', print_R},
-		{'r', print_r}, {'\0', NULL}
+		{'r', print_r}, {'p', print_p}, xsxc{'\0', NULL}
 	};
 	for (c1 = 0; l[c1].c != '\0'; c1++)
 	{
